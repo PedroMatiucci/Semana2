4573e027905f1f3d6adaf74406fcb1730b1b734e
Pedro Pereira <pmatiucci@gmail.com>
2022-11-21 18:37:53 -0300

    Exercicio4 completo

---
+++ Semana2/exercicio4/Certidao.cs
@@ -0,0 +1,16 @@
+﻿namespace Semana2.exercicio4
+{
+    internal class Certidao
+    {
+        DateTime dataEmissao;
+        Pessoa pessoa;
+
+        public Certidao(DateTime dataEmissao, Pessoa pessoa)
+        {
+            this.dataEmissao = dataEmissao;
+            this.pessoa = pessoa;
+        }
+
+        public DateTime DataEmissao { get { return dataEmissao; } private set { } }
+    }
+}
---
+++ Semana2/exercicio4/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana2.exercicio4
+{
+    internal class Pessoa
+    {
+        string nome;
+
+        public Pessoa(string nome) {
+            this.nome = nome;
+        }
+    }
+}
