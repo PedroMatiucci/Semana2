65095d34eac874a91ae400eb582383b6c25ba3bf
Pedro Pereira <pmatiucci@gmail.com>
2022-11-22 17:50:03 -0300

    Exercicio7 completo

--- Semana2/Program.cs
+++ Semana2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Semana2.exercicio6;
+using Semana2.exercicio7;
 
 namespace main
 {
@@ -7,7 +8,7 @@ namespace main
 
         static void Main(string[] args)
         {
-
+            /*
             // imprime As Progressoes 10x
             ProgressaoAritmetica pa = new ProgressaoAritmetica(3, 4);
             ProgressaoGeometrica pg = new ProgressaoGeometrica(3, 4);
@@ -18,6 +19,15 @@ namespace main
                 Console.WriteLine("PG = {0}",
                     pg.ProximoValor);
             }
+            */
+            Propriedades teste = new Propriedades(@"C:\Users\Carla\\teste\propriedades.txt");
+            string valor = teste.RetornaValor("teste");
+            teste.AlterarValor("teste", "mudei o valor2");
+            Console.WriteLine(valor);
+            teste.EscreveDados();
+            //teste.AlterarValor("teste", "teste1");
+            //teste.AlterarValor("teste2", "teste2");
+            //teste.EscreveDados();
 
 
         }
---
+++ Semana2/exercicio7/Propriedades.cs
@@ -0,0 +1,82 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System;
+
+namespace Semana2.exercicio7
+{
+    internal class Propriedades
+    {
+        Dictionary<string, string> dicPropriedades = new Dictionary<string, string>();
+        string path;
+        FileStream stream;
+
+        public Propriedades(string path = "")
+        {
+            this.path = path;
+            try
+            {
+                string i = "0";
+                string[] lines = File.ReadAllLines(path, Encoding.UTF8);
+                foreach (string line in lines)
+                {
+                    int index = line.IndexOf("=");
+                    string key = line.Substring(0, index);
+                    string value = line.Substring(index + 1);
+                    DicPropriedades.Add(key, value);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Path nao existente");
+            }
+
+        }
+        public Dictionary<string, string> DicPropriedades { get { return dicPropriedades; } }
+
+        public dynamic RetornaValor(string chave)
+        {
+            string value;
+            if (DicPropriedades.TryGetValue(chave, out value))
+            {
+                return DicPropriedades[chave];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void AlterarValor(string chave, string valor)
+        {
+            if (DicPropriedades.ContainsKey(chave))
+            {
+                DicPropriedades[chave] = valor;
+            }
+            else
+            {
+                DicPropriedades.Add(chave, valor);
+            }
+        }
+
+        public bool VerificaChave(string chave)
+        {
+            return DicPropriedades.ContainsKey(chave);
+        }
+
+        public void EscreveDados()
+        {
+            StreamWriter File = new StreamWriter(@"C:\\Users\\Carla\\Desktop\\teste\propriedades.txt");
+            foreach(KeyValuePair<string,string> dado in DicPropriedades)
+            {
+                string texto = dado.Key + "=" + dado.Value;
+                File.WriteLine(texto);
+            }
+            File.Close();
+
+        }
+    }
+}
+
+
+
